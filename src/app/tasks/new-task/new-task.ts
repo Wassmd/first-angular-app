849e381b02dfae8f356873676669046f08677512
Wasim <mohammed.wasimuddin@rewe-group.com>
2025-09-16 11:35:57 +0200

    Form input binding

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,16 +1,24 @@
 import {Component, EventEmitter, Output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.html',
   styleUrl: './new-task.css'
 })
 export class NewTask {
-
   @Output() close = new EventEmitter();
 
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+
   onCloseAddNewTask() {
+    console.log(this.enteredTitle);
+    console.log(this.enteredSummary);
+    console.log(this.enteredDueDate);
+
     this.close.emit();
   }
 }
