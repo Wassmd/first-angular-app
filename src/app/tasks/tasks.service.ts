410356eb8375474fb0bbddbc34cfd777bae9ae81
Wasim <mohammed.wasimuddin@rewe-group.com>
2025-09-16 16:12:53 +0200

    Save in local storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -5,31 +5,8 @@ import {Injectable} from '@angular/core';
   providedIn: 'root'
 })
 export class TasksService {
-   private tasks: Atask[] = [
-    {
-      id: 't1',
-      userId: 'u1',
-      title: 'Master Angular',
-      summary:
-        'Learn all the basic and advanced features of Angular & how to apply them.',
-      dueDate: '2025-12-31',
-    },
-    {
-      id: 't2',
-      userId: 'u3',
-      title: 'Build first prototype',
-      summary: 'Build a first prototype of the online shop website',
-      dueDate: '2024-05-31',
-    },
-    {
-      id: 't3',
-      userId: 'u3',
-      title: 'Prepare issue template',
-      summary:
-        'Prepare and describe an issue template which will help with project management',
-      dueDate: '2024-06-15',
-    },
-  ];
+   private tasks: Atask[] = localStorage.getItem('tasks') ? JSON.parse(localStorage.getItem('tasks')!) : [];
+
 
   getTasks(userId: string): Atask[] {
     return this.tasks.filter(task => task.userId === userId);
@@ -37,9 +14,15 @@ export class TasksService {
 
   addTask(task: Atask) {
     this.tasks.push(task);
+    this.saveTasks();
   }
 
   deleteTask(taskId: string) {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
